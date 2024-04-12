4932904c7a74e2d093da35704624d9c605636263
ciao1092 <carlo.condo@outlook.com>
2024-04-12 16:33:40 +0200

    Remove unused imports

--- src/main/java/net/ccond/RPN/Main.java
+++ src/main/java/net/ccond/RPN/Main.java
@@ -3,9 +3,7 @@ package net.ccond.RPN;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.EmptyStackException;
-import java.util.IllegalFormatConversionException;
 import java.util.Stack;
 
 public class Main {
